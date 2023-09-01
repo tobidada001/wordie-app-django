7d2267b9653ca61c306b984b64202d10ba0962bb
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-09-01 22:56:06 +0100

    Set debug to false

--- WordieProject/settings.py
+++ WordieProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hottrzne@#$d0t8gix*)^zmj0=xp2+!%zf8yux$6ozjgg1ef&q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
