b5804479807c1062bc80579bf1415fdaa627806f
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-09-01 23:59:10 +0100

    I fixed static error

--- WordieProject/settings.py
+++ WordieProject/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'wordieapp/static']
+STATICFILES_DIRS = [BASE_DIR/'WordieApp/static']
 STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
