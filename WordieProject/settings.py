15671d80decb8013481de901c6dc348ec192494c
OLUWATOBI <dadaoluwatobijohn@gmail.com>
2023-09-01 23:38:54 +0100

    I added static root

--- WordieProject/settings.py
+++ WordieProject/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'wordieapp/static']
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
